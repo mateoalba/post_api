3d6b24b80afa04c2c031c2e4da5f5af4228e4d69
msteoalba <mateoalba1234gmail.com>
2025-12-17 21:58:39 -0500

    added health endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World desde los Poderosos Programadores del Tercer Semestre!';
+  getHello(): any {
+    return {
+      success: true,
+      message: 'martinez-posts-api on line',
+      data: 'Sistema Posts API',
+      version: '1.0.0',
+    }
   }
 }
